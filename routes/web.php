60f751829ba1214d553f7791ba124009494e4088
sugihiro1 <sugiyama@k00.itscom.net>
2018-08-19 08:00:36 +0000

    rakuten api

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,12 @@ Route::get('/', 'WelcomeController@index');
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
+// ログイン認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('items', 'ItemsController', ['only' => ['create']]);
+});
+
